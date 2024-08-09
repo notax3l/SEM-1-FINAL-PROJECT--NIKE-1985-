8afe0fba6425d7414e66f5197448ea19433091d2
Tanishq Pratap Singh <135806783+notax3l@users.noreply.github.com>
2024-08-08 20:16:28 -0400

    Add files via upload

---
+++ NIKE-1985/scripts.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+    //this function will load all of our headers and footer related files into our main HTML
+    $(function () {
+        $("#meta").load("meta.html");
+        $("#header").load("header.html");
+        $("#footer").load("footer.html");
+
+    });
+    // here we will find the dimensions of the div with the id div1
+
+
+
+});
